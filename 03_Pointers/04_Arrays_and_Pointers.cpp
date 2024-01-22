98768720c2a346bcc5a8c8904589ba86b6282780
Yashaswi <mishrayashaswikumar@gmail.com>
2024-01-22 20:29:02 +0530

    pointers

--- 03_Pointers/04_Arrays_and_Pointers.cpp
+++ 03_Pointers/04_Arrays_and_Pointers.cpp
@@ -9,9 +9,10 @@ int main()
 
     int *score_ptr{scores}; //no need to add & as scores is the address of first element of scores array
     cout<<*score_ptr<<endl;//100
+    cout<<*scores<<", "<<*score_ptr<<", "<<scores[0]<<", "<<score_ptr[0]<<" are equal"<<endl;
     cout<<score_ptr[2]<<" and "<<scores[2]<< " are equal "<<endl;
-    cout<<*(score_ptr+2)<<endl;//89
-    //scores[n], score_ptr[n] and *(score_ptr+n) can be used interchangeably
+    cout<<*(score_ptr+2)<<" and "<<*(scores+2)<< " are equal "<<endl;
+    //scores[n], score_ptr[n], *(scores+n) and *(score_ptr+n) can be used interchangeably
 
 
 
