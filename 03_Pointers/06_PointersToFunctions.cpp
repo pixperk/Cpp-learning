bf8c94f58fc6eb48a0b85b180ed446cef72ffa69
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-01-23 20:10:25 +0530

    saved changes

--- 03_Pointers/06_PointersToFunctions.cpp
+++ 03_Pointers/06_PointersToFunctions.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void double_data(int *ptr){
-    *ptr *=2; 
+void double_data(int *ptr)
+{
+    *ptr *= 2;
 }
 int main()
 {
     int val{10};
     int *pointer{nullptr};
-    
 
- double_data(&val);
- cout<<val;
- cout<<endl;
- pointer = &val;
+    double_data(&val);
+    cout << val;
+    cout << endl;
+    pointer = &val;
     double_data(pointer);
-    cout<<val;
- return 0;
+    cout << val;
+    return 0;
 }
