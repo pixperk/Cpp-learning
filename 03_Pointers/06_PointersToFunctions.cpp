54a16c053a716284c51b248133a707d234f7fb88
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-01-23 20:10:16 +0530

    func&pointers

---
+++ 03_Pointers/06_PointersToFunctions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void double_data(int *ptr){
+    *ptr *=2; 
+}
+int main()
+{
+    int val{10};
+    int *pointer{nullptr};
+    
+
+ double_data(&val);
+ cout<<val;
+ cout<<endl;
+ pointer = &val;
+    double_data(pointer);
+    cout<<val;
+ return 0;
+}
